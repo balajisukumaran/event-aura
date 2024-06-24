f5f990e362f3558029a07b0548e60c19a0157804
sruthi_shaji <sruthii.mec@gmail.com>
2024-06-24 03:27:14 -0300

    feat: minor changes changes for removing unwanted code

--- frontend/src/components/Navbar/NavBar.js
+++ frontend/src/components/Navbar/NavBar.js
@@ -6,16 +6,10 @@ import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isProfileOpen, setIsProfileOpen] = useState(false);
     const navigate = useNavigate();
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
-        setIsProfileOpen(false);
-    };
-
-    const toggleProfileMenu = () => {
-        setIsProfileOpen(!isProfileOpen);
     };
 
     return (
@@ -29,16 +23,6 @@ const NavBar = () => {
                 <p onClick={() => navigate("/contact")}>Contact Us</p>
 
                 <p><button className='login-register-button' onClick={() => navigate("/login")}>Login</button> </p>
-                {/* <div className="profile-icon" onClick={toggleProfileMenu}>
-                    <AccountCircleTwoToneIcon fontSize="large" style={{ color: "#FF9A00" }} />
-                    {isProfileOpen && (
-                        <div className="profile-dropdown">
-                            <Link to="/profile">Profile</Link>
-                            <Link to="/settings">Settings</Link>
-                            <Link to="/logout">Logout</Link>
-                        </div>
-                    )}
-                </div> */}
             </div>
             <div className="navbar-menu-icon" onClick={toggleMenu}>
                 <MenuIcon fontSize="large" style={{ color: "#FF9A00" }} />

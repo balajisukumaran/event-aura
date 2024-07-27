be2fedfae38762e6d11ae78b97a40b8f780524c2
Kabilesh <kabilesh@dal.ca>
2024-07-26 22:27:16 -0300

    event history frontend

--- frontend/src/pages/EventHistory/EventHistory.jsx
+++ frontend/src/pages/EventHistory/EventHistory.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import EventSlider from "../../components/EventSlider";
+import { Tab, Tabs } from "@mui/material";
+import { events, tabs } from "./constants";
 import './style.scss';
 
 const EventHistory = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  
+  const handleChange = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
+
   return (
-    <div>
-      Event History
+    <div className="event-history">
+      <Tabs TabIndicatorProps={{style: {background:'#FF9A00'}}} className="event-history-tabs" centered value={selectedTab} onChange={handleChange}>
+        {tabs.map((tab) => <Tab className="event-history-tabs-item" label={tab.name} key={tab.id} sx={{ '&.Mui-selected': { color: 'red' } }} />)}
+      </Tabs>
+      <EventSlider events={events} className="event-history-slider" />
     </div>
   )
 };

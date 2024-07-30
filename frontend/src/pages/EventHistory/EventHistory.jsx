f42d2653fed08fcbe9a12fdd23891b419ea2501d
Kabilesh <kb320903@dal.ca>
2024-07-30 19:02:12 -0300

    placeholder

--- frontend/src/pages/EventHistory/EventHistory.jsx
+++ frontend/src/pages/EventHistory/EventHistory.jsx
@@ -41,8 +41,8 @@ const EventHistory = () => {
       <Tabs TabIndicatorProps={{style: {background:'#FF9A00'}}} className="event-history-tabs" centered value={selectedTab} onChange={handleChange}>
         {tabs.map((tab) => <Tab className="event-history-tabs-item" label={tab.name} key={tab.id} />)}
       </Tabs>
-      {createdEvents?.length && selectedTab === 0 ? <EventSlider events={createdEvents} className="event-history-slider" /> : null}
-      {bookedEvents?.length && selectedTab === 1 ? <EventSlider events={bookedEvents?.map((bookedEvent) => ({...bookedEvent?.event, onCancelBooking: () => onCancelBooking(bookedEvent?.id)}))} className="event-history-slider" /> : null}
+      {selectedTab === 0 && (createdEvents?.length ? <EventSlider events={createdEvents} className="event-history-slider" /> : <div className="event-history-placeholder">You didn't create any events</div>) }
+      {selectedTab === 1 && ( bookedEvents?.length ? <EventSlider events={bookedEvents?.map((bookedEvent) => ({...bookedEvent?.event, onCancelBooking: () => onCancelBooking(bookedEvent?.id)}))} className="event-history-slider" /> : <div className="event-history-placeholder">You didn't book any events</div>) }
     </div>
   )
 };

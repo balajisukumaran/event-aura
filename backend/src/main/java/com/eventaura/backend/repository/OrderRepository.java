929409e8a573ccd341b4f7a22da2c58a658542d5
sukumaran <bl664064@dal.ca>
2024-07-30 19:23:36 -0300

    with merge event-history changes

--- backend/src/main/java/com/eventaura/backend/repository/OrderRepository.java
+++ backend/src/main/java/com/eventaura/backend/repository/OrderRepository.java
@@ -3,11 +3,10 @@ package com.eventaura.backend.repository;
 import com.eventaura.backend.entity.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
-@Repository
-
 import java.util.List;
 
+
+@Repository
 public interface OrderRepository extends MongoRepository<Order, String> {
     List<Order> getOrdersByUserId(String userId);
 }

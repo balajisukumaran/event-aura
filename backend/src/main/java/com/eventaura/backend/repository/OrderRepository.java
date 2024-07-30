1e2c44a573ee20663e54435483094044f6c0ab7e
Kabilesh <kb320903@dal.ca>
2024-07-30 19:10:58 -0300

    event history

--- backend/src/main/java/com/eventaura/backend/repository/OrderRepository.java
+++ backend/src/main/java/com/eventaura/backend/repository/OrderRepository.java
@@ -4,9 +4,10 @@ import com.eventaura.backend.entity.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 
-import java.util.List;
 
 public interface OrderRepository extends MongoRepository<Order, String> {
     List<Order> getOrdersByUserId(String userId);

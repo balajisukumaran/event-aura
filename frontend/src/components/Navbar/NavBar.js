376390b62877db745f12c61a274b926a4b2ea6d8
sukumaran <bl664064@dal.ca>
2024-07-30 21:47:17 -0300

    fix merge conflict

--- frontend/src/components/Navbar/NavBar.js
+++ frontend/src/components/Navbar/NavBar.js
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
+import UserDropdown from "../UserDropdown/UserDropdown";
 
 const NavBar = () => {
+  const [role, setRole] = useState(null);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
+  const token = localStorage?.getItem("token");
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    const storedRole = localStorage.getItem("role");
+    if (!storedRole) {
+      navigate("/login");
+    } else {
+      setRole(storedRole);
+    }
+  }, []);
 
   return (
     <nav className="navbar">
@@ -20,36 +37,57 @@ const NavBar = () => {
         </Link>
       </div>
       <div className="navbar-links">
-        <p onClick={() => navigate("/event-history")}>Events</p>
+        {role !== "ADMIN" && (
+          <p onClick={() => navigate("/event-history")}>Events</p>
+        )}
         <p onClick={() => navigate("/faq")}>FAQs</p>
         <p onClick={() => navigate("/contact")}>Contact Us</p>
+        {role === "ADMIN" && (
+          <p onClick={() => navigate("/approvals")}>Approvals</p>
+        )}
 
-        <p>
-          <button
-            className="login-register-button"
-            onClick={() => navigate("/login")}
-          >
-            Login
-          </button>{" "}
-        </p>
+        {!token ? (
+          <p>
+            <button
+              className="login-register-button"
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </button>
+          </p>
+        ) : (
+          <UserDropdown handleLogout={handleLogout} />
+        )}
       </div>
       <div className="navbar-menu-icon" onClick={toggleMenu}>
         <MenuIcon fontSize="large" style={{ color: "#FF9A00" }} />
       </div>
       {isMenuOpen && (
         <div className="navbar-dropdown">
-          <Link to="/login" onClick={toggleMenu}>
-            Events
-          </Link>
+          {role !== "ADMIN" && (
+            <Link to="/event-history" onClick={toggleMenu}>
+              Events
+            </Link>
+          )}
           <Link to="/faq" onClick={toggleMenu}>
             FAQ
           </Link>
           <Link to="/contact" onClick={toggleMenu}>
             Contact
           </Link>
-          <Link to="/login" onClick={toggleMenu}>
-            Login
-          </Link>
+          {role === "ADMIN" && (
+            <Link to="/approvals" onClick={toggleMenu}>
+              Approvals
+            </Link>
+          )}
+
+          {!token ? (
+            <Link to="/login" onClick={toggleMenu}>
+              Login
+            </Link>
+          ) : (
+            <UserDropdown />
+          )}
         </div>
       )}
     </nav>

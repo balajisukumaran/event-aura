0d49e203621b22b1d075ad27cf654537657e895b
sruthi_shaji <sruthii.mec@gmail.com>
2024-07-30 22:57:57 -0300

    feat: issues fixed UI

--- backend/src/main/java/com/eventaura/backend/configuration/WebConfig.java
+++ backend/src/main/java/com/eventaura/backend/configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://main--event-aura.netlify.app")
+                .allowedOrigins("http://localhost:3000", "https://event-aura.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
